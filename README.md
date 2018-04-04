a14fb9182608c046bda199de781fba3750c01065
Samantha Kemps <samanthakemps@Samanthas-MacBook-Pro.local>
2018-04-03 20:32:38 -0400

    added edited files

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# kemps_samantha_GOT
+HTML 5 video with JS / ES6
+ simple video app / GOT house explainer with video and css animation.
+
+ using native video and video events with ES6
+
+
+ touch .gitignore
+ nano .gitignore
+ *.mp4
+ *.webm
+ *.ogv
+ .DS_Store
+ ctrl+O+enter
+ ctrl+X
---
+++ js/main.js
@@ -0,0 +1,83 @@
+(() => {
+console.log("got videos");
+
+//add to string prototype to cap first letter
+String.prototype.capIt = function() {return this.replace(this.charAt(), this.charAt().toUpperCase());};
+
+const sigils = document.querySelectorAll('.sigilContainer'),
+	  lightbox = document.querySelector('.lightbox'),
+	  closeLightbox = lightbox.querySelector('.close-lightbox'),
+	  vidPlayer = document.querySelector('video'),
+		tagline = document.querySelector('h1'),
+		playPause = document.querySelector('.play-pause'),
+		ffWd = document.querySelector('.forward'),
+		rWnd = document.querySelector('.rewind'),
+		imageBanner = document.querySelector('#houseImages');
+
+
+//methods/ functions
+function loadMovie(){
+	//1.turn on light box
+	lightbox.classList.add('show-lightbox');
+
+//2.grab the right video based on class name- split yields name
+	var house = this.className.split(' ')[1].capIt();
+	tagline.innerHTML = house;
+
+//3. put the path toegther and make the video load and play
+
+	vidPlayer.src = `videos/House-${house}.${vidPlayer.currentSrc.split('.')[1]}`;
+
+	vidPlayer.load();
+	vidPlayer.play();
+
+	animateBanners(this.dataset.offset);
+}
+function animateBanners(offset){
+	//debugger;
+	console.log(600 * offset);
+
+	imageBanner.style.right = (offset * 600) + "px";
+}
+
+function closeLBox(){
+	lightbox.classList.remove('show-lightbox');
+	vidPlayer.pause();
+	vidPlayer.currentTime=0;
+}
+
+function togglePlay(){
+	//debugger;
+	var theSVG = this.firstElementChild;
+
+	if (vidPlayer.paused) {
+		vidPlayer.play();
+		theSVG.dataset.icon = "pause-circle";
+	}
+	else{
+		vidPlayer.pause();
+		theSVG.dataset.icon = "play-circle";
+	}
+
+}
+
+function ffWdVid(){
+	debugger;
+}
+
+function rWndVid(){
+	debugger;
+}
+
+//events
+sigils.forEach(sigil => sigil.addEventListener('click', loadMovie));
+closeLightbox.addEventListener('click', closeLBox);
+
+vidPlayer.addEventListener('ended',closeLBox);
+
+playPause.addEventListener('click', togglePlay);
+
+rWnd.addEventListener('click', rWndVid);
+
+ffWd.addEventListener('click', fWndVid);
+})();
